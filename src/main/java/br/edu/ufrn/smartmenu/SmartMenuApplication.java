b9a40497a60a7c12208c030db74b0496625e5d03
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2024-10-10 12:06:10 -0300

    initial structure of java project with spring boot using gradle

---
+++ src/main/java/br/edu/ufrn/smartmenu/SmartMenuApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufrn.smartmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ufrn/smartmenu/SmartmenuApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufrn.smartmenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartmenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
